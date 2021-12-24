aa57978a1e0ba0e207ac3457d00a1cd5d4319af5
Pushkar Singh <stoazest@gmail.com>
2021-12-24 11:20:58 +0530

    The code is connected to the API

--- app.js
+++ app.js
@@ -2,12 +2,47 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output")
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+// Processing
+function urlGenerator(text) {
+    return  serverUrl + "?text=" + text;
+}
+function errorHandler(error){
+    console.log("Error occured", error)
+    alert("Something went wrong, please try again later");
+}
 
 // Adding Event listener
 btnTranslate.addEventListener("click", function eventHandler() {
-    txtOutput.innerHTML = txtInput.value;
-
+    var url = urlGenerator(txtInput.value)
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        {
+            var translatedText = json.contents.translated
+            txtOutput.innerHTML = translatedText;
+        }
+    })
+    .catch(errorHandler)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Live Exercise  --> Query Selector
